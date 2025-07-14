2e25c828057c821dc73105d018ff88a7722c7bff
anusha-1907 <anushasagadabona@gmail.com>
2025-07-14 16:47:28 +0530

    adding files

---
+++ flipkart.rbg2.java
@@ -0,0 +1,8 @@
+public class flipkartrbg
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello");
+	}
+}
+
---
+++ flipkart.rbg3.java
@@ -0,0 +1,7 @@
+public class flipkart3
+{
+	public static void main(String args[])
+	{
+		System.out.println("java");
+	}
+}
